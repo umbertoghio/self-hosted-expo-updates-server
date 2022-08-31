d32e86dc73624109ab2f2f58848879b5527dfb6c
Umberto Ghio <umberto@ghio.io>
2022-09-01 01:59:11 +0200

    Improved headers & params parsing

--- API/src/modules/expo/request.js
+++ API/src/modules/expo/request.js
@@ -1,22 +1,22 @@
 const Err = require('@feathersjs/errors')
 
 module.exports.getRequestParams = ({ query, headers }) => {
-  const project = headers['expo-project'] ?? query.project
+  const project = query.project ?? headers['expo-project']
   if (!project || typeof project !== 'string') {
     throw new Err.BadRequest('No expo-project header or project qyery provided.')
   }
 
-  const platform = headers['expo-platform'] ?? query.platform
+  const platform = query.platform ?? headers['expo-platform']
   if (platform !== 'ios' && platform !== 'android') {
     throw new Err.BadRequest('Missing expo-platform header or platform qyery provided. Expected either ios or android.')
   }
 
-  const runtimeVersion = headers['expo-runtime-version'] ?? query['runtime-version']
+  const runtimeVersion = query.version ?? headers['expo-runtime-version']
   if (!runtimeVersion || typeof runtimeVersion !== 'string') {
     throw new Err.BadRequest('Missing expo-runtime-version header or runtime-version qyery provided.')
   }
 
-  const releaseChannel = headers['expo-channel-name'] ?? query.channel
+  const releaseChannel = query.channel ?? headers['expo-channel-name']
   if (!releaseChannel || typeof releaseChannel !== 'string') {
     throw new Err.BadRequest('Missing expo-channel-name header orchannel-name qyery provided.')
   }
