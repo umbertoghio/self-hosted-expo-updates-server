038c52eca343b91b373a72df95f37aac526d11a7
Umberto Ghio <umberto@ghio.io>
2025-01-31 15:17:18 +0100

    Better error handling

--- API/src/index.js
+++ API/src/index.js
@@ -14,14 +14,19 @@ app.configure(feathersconfig(express))
 const server = app.listen(app.get('port'))
 
 const createAdminIfMissing = async () => {
-  await new Promise(resolve => setTimeout(resolve, 1000))
-  const [admin] = await app.services.users.find({ query: { username: 'admin' } })
-  if (!admin) app.services.users.create({ username: 'admin', password: app.get('adminPass'), role: 'admin' })
+  try {
+    if (!process.env.MONGO_CONN) throw new Error('MONGO_CONN not defined, please run this server under docker compose or set MONGO_CONN env variable')
+    const [admin] = await app.services.users.find({ query: { username: 'admin' } })
+    if (!admin) app.services.users.create({ username: 'admin', password: app.get('adminPass'), role: 'admin' })
+  } catch (e) {
+    logger.error(e.message)
+    logger.error('Error creating admin user, please try to restart API server or verify Mongodb connection.', e)
+  }
 }
 server.on('listening', () => {
   logger.info(`Feathers application started on http://${app.get('host')}:${app.get('port')}`)
   logger.info(`Env: ${process.env.NODE_ENV} DB: ${app.get('mongodb')}`)
-  createAdminIfMissing()
+  setTimeout(createAdminIfMissing, 3000)
 })
 
 // Docker support
